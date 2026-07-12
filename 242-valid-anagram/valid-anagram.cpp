4b99b6ce042a3a63082c81b5b386c4f58fd707da
adityakvhsn-ui <adityakv.hsn@gmail.com>
2026-07-12 14:32:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.80%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        	//your code goes here
+            if(s.length()!=t.length())
+            return false;
+            int freq[26]={0};
+            int n=s.length();
+            for(int i=0;i<n;i++)
+            {
+                freq[s[i]-'a']++;
+                freq[t[i]-'a']--;
+            }
+            for(int i=0;i<26;i++)
+            {
+                if(freq[i]!=0)
+                return false;
+            }
+            return true;
+    }
+};
