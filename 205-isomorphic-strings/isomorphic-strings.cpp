18ac9429a84d08f2e20174ded35cad364747af87
adityakvhsn-ui <adityakv.hsn@gmail.com>
2026-07-12 13:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (29.18%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+         int smap[256]={0},tmap[256]={0};
+        int n=s.size();
+
+        for(int i=0;i<n;i++)
+        {
+            if(smap[s[i]]!=tmap[t[i]])
+            return false;
+
+            smap[s[i]]=i+1;
+            tmap[t[i]]=i+1;
+        }
+        return true;
+    }
+};
